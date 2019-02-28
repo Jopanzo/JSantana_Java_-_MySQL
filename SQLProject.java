75e6ad046bb704f79fd8573d1ba8951f6231648e
Jopanzo <jpnzo2k12@gmail.com>
2019-02-28 02:04:07 -0500

    Add files via upload

---
+++ SQLProject.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sqlproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author johnn
+ */
+
+public class SQLProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try 
+        {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sample1?autoReconnect=true&useSSL=false","root","Botgarden12!");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+             stmt = conn.createStatement();
+             rs = stmt.executeQuery("SELECT * FROM patientbed");
+             while(rs.next())
+             {
+                 int id = rs.getInt("PatientID");
+                 int bed = rs.getInt("BedID");     
+        // print the results
+        System.out.format("%s, %s\n", id, bed);
+             }
+             stmt.close();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            // handle the error         
+        }
+          catch (Exception ex) {
+            // handle the error
+        }
+        finally 
+        {
+        if (stmt != null) { stmt.close(); }
+        }
+    }
+    
+}
